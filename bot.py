ffdef4c9051f9efb84ba74b2911a569bdcc11fcd
Angel Garcia <101078753+angel012912@users.noreply.github.com>
2024-05-05 10:52:26 +0000

    Main bot function implementation. Its missing the model integration

--- bot.py
+++ bot.py
@@ -3,17 +3,29 @@
 from selenium_scraper.scraper.twitter_scraper import Twitter_Scraper
 import os
 import sys
+import random
+import tweepy
+import time
 
 from dotenv import load_dotenv
 load_dotenv()
 
 class Bot:
 
-    def __init__(self, model_weight_path="", database_data={}):
-        #self.model = Model(model_weight_path) 
+    TWEET_CONTENTS = {1: "I'm sorry to hear that. Please contact our customer service at 1-800-123-4567", 2: "I'm glad you're happy with our service. We're always here to help you."}
+
+    def __init__(self, model_weight_path="", database_data={}, sleep_time=300):
+        #self.model = Model(model_weight_path)
         #self.preprocessing = Preprocessing()
         self.database_data = database_data
         self.tweets_to_read = {}
+        self.sleep_time = sleep_time
+        self.tweepy_client = tweepy.Client(
+            consumer_key=os.getenv("TWITTER_API_KEY"),
+            consumer_secret=os.getenv("TWITTER_API_SECRET"),
+            access_token=os.getenv("TWITTER_ACCESS_TOKEN"),
+            access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+        )
     
     def _call_scraper(self, **kwargs):
         USER_MAIL = kwargs["mail"] if "mail" in kwargs else os.getenv("TWITTER_MAIL")
@@ -68,29 +80,54 @@ class Bot:
             )
             sys.exit(1)
 
-    def monitor_tweets(self):
-        self._call_scraper(tweets=10, query="heybanco (from:AngelGa35156959)")
-        self._read_tweets_from_csv()
-        print(self.tweets_to_read) # Debugging purposes
-
-    
     def _read_tweets_from_csv(self):
         tweets_folder_path = os.path.join(os.getcwd(), "tweets")
         for file in os.listdir(tweets_folder_path):
             if file.endswith(".csv"):
                 with open(os.path.join(tweets_folder_path, file), "r") as f:
                     file_data = f.readlines()
-                    for line in file_data:
+                    for index, line in enumerate(file_data):
+                        if index == 0:
+                            continue
                         column_data = line.split(",")
                         tweet_id = column_data[-1]
-                        timestamp = column_data[2].split("t")
-                        date = timestamp[0]
-                        time = timestamp[1]
-                        tweet_text = column_data[4]
-                        self.tweets_to_read[tweet_id] = { 'date': date, 'time': time, 'tweet_text': tweet_text}
-                #os.remove(os.path.join(tweets_folder_path, file))
+                        if tweet_id not in self.database_data:
+                            timestamp = column_data[2].split("t")
+                            date = timestamp[0]
+                            time = timestamp[1]
+                            tweet_text = column_data[4]
+                            self.tweets_to_read[tweet_id] = {'date': date, 'time': time, 'tweet_text': tweet_text}
+                os.remove(os.path.join(tweets_folder_path, file))
         return self.database_data
-    
+
+    def _dummy_evaluate(self, tweet):
+        return random.choice([0, 1, 2])
+
+    def _get_actual_tweets(self):
+        # self._call_scraper(tweets=10, query="heybanco (from:zAngelGa35156959)")
+        self._call_scraper(tweets=10, username="elonmusk")
+        self._read_tweets_from_csv()
+        print(self.tweets_to_read) # Debugging purposes
+
+    def _post_tweet_response(self, evaluation, tweet_id):
+        response = self.TWEET_CONTENTS[evaluation]
+        self.tweepy_client.create_tweet(text=response, in_reply_to_tweet_id=tweet_id)
+
+    def _evaluate_tweets(self):
+        for tweet_id, tweet_data in self.tweets_to_read.items():
+            tweet_text = tweet_data['tweet_text']
+            tweet_evaluation = self._dummy_evaluate(tweet_text)
+            if tweet_evaluation != 0:
+                self._post_tweet_response(tweet_evaluation, tweet_id)
+            self.database_data[tweet_id] = { 'date': tweet_data['date'], 'time': tweet_data['time'], 'tweet_text': tweet_text, 'evaluation': tweet_evaluation}
+        self.tweets_to_read = {}
+        return self.database_data
+
+    def run_bot(self):
+        self._get_actual_tweets()
+        self._evaluate_tweets()
+        time.sleep(self.sleep_time)
+
 if __name__ == "__main__":
     bot = Bot()
-    bot.monitor_tweets()
+    bot.run_bot()
